c2449a7771e0eec2c7289779c7b0037595c6cf30
YouHyunSu <gustn3181@naver.com>
2020-01-13 21:14:38 +0900

    전화번호부

--- part-28/javascripts/script.js
+++ part-28/javascripts/script.js
@@ -70,10 +70,12 @@ $('.ok').on('click',function(){
 //     var plus1 = '<tr><td>' + str1 + '</td><td>' + str2 + '</td></tr>'
 //     $('tbody').append(plus1);
     
-    
+var selected = -1;
     
 // });
 function addPhone(){
+
+   
     var name = $('.name').val();
     var phone = $('.phone').val();
 
@@ -100,22 +102,30 @@ function addPhone(){
 
 // 2. data.js의 데이터를 먼저 보여주고 기능을 진행한다.
 // 데이터가 기본이 되야 한다.
+
+
+
 function viewList(){
 
     $('tbody').empty();
     for( var i =0; i < data.list.length; i++) {
-
-    var tag = '<tr>\
-        <td>' + data.list[i].name + '</td>\
-        <td>' + data.list[i].phone + '</td>\
+        // '<tr class="idx' + i +'">\
+    var tag = '<tr class="idx' + i +'" data-idx="' + i + '">\
+        <td class="name">' + data.list[i].name + '</td>\
+        <td class="phone">' + data.list[i].phone + '</td>\
         <td class="remove">삭제</td>\
-        <td class="modify">수정</td>\
+        <td class="select">수정</td>\
     </tr>';
 
     $('tbody').append(tag);
 }
 };
 function newFunc(){
+    if( selected >= 0 ) {
+        alert('수정을 위한 값입니다. 등록을 원할 경우 새로운 값을 입력해주세요');
+        return;
+    }
+
         var name = $('.name').val();
         var phone = $('.phone').val();
     
@@ -148,27 +158,64 @@ function removeItem(){
     // react 메모리상의 변동이 없으면 그대로 사용하는 방식
 }
 
-function updateItem(){
-    var nam = $('.name').val();
-    var phon = $('.phone').val();
-    console.log(nam);
-    console.log(phon);
+// function updateItem(){
+//     var nam = $('.name').val();
+//     var phon = $('.phone').val();
+//     var idx = $(this).parent().index();
+//     var up = data.list[idx]
     
-}
-function modifyItem(){
-    var idx = $(this).parents().index();
-    var up = data.list[idx]
-    console.log(up.name);
-    console.log(up.phone);
-    $('.name').val(up.name);
-    $('.phone').val(up.phone);
-    viewList();
-}
+//     console.log(up.name);
+    
+// }
+// function modifyItem(){
+//     var idx = $(this).parent().index();
+//     var up = data.list[idx]
+//     console.log(up.name);
+//     console.log(up.phone);
+//     $('.name').val(up.name);
+//     $('.phone').val(up.phone);
+//     viewList();
+// }
 $('.add').on('click',newFunc);
 $(document).on('click','.remove',removeItem);
-$(document).on('click','.update',updateItem);
-$(document).on('click','.modify',modifyItem);
+// $(document).on('click','.update',updateItem);
+// $(document).on('click','.modify',modifyItem);
 viewList();
 
 // 수정하는 기능 만들기
 // 수정하기 누르면 이름 폰의 값을 input 값에 넣고 다시 위의 수정 버튼을 누르면 바꾸는 방식
+// index를 key로 쓸수 있는 것
+// index는 음수가 나오지 않는다는 것을 이용하자
+$(document).on('click','.select',function(){
+    
+    // var idx = $(this).parent().index();
+    // var idx =$(this).parent().attr('class').replace('idx',''); // idx0 // replace는 한번만 작동되기 때문에 전체를 하는 것은 만들어야 한다.
+    // replace에서 정규표현식을 사용하면 전체 변경이 가능하다.
+    var idx = $(this).parent().data('idx');
+    console.log(idx);
+    selected = idx;
+
+    var name = $('.name');
+    var phone = $('.phone');
+    
+    var a = $(this).siblings('.name').text()
+    var b = $(this).siblings('.phone').text()
+    // name.val(data.list[idx].name);
+    // phone.val(data.list[idx].phone);
+    name.val(a);
+    phone.val(b);
+    
+
+    // alert('수정이 선택이 되었습니다.')
+});
+
+$('.edit').on('click',function(){
+    var name = $('.name').val();
+    var phone = $('.phone').val();
+
+    data.list[selected].name = name;
+    data.list[selected].phone = phone;
+
+    selected = -1;
+    viewList();
+});
